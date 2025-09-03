2e134101a1f6249ba212f83ade64eff8e60472db
Prince Jaiswal <pj1308109@gmail.com>
2025-09-03 23:56:25 +0530

    Update thread.tsx

--- my-ai/components/assistant-ui/thread.tsx
+++ my-ai/components/assistant-ui/thread.tsx
@@ -87,7 +87,7 @@ const ThreadWelcomeSuggestions: FC = () => {
     <div className="mt-3 flex w-full items-stretch justify-center gap-4">
       <ThreadPrimitive.Suggestion
         className="hover:bg-muted/80 flex max-w-sm grow basis-0 flex-col items-center justify-center rounded-lg border p-3 transition-colors ease-in"
-        prompt="What is the weather in Tokyo?"
+        prompt="What is the weather in New Delhi?"
         method="replace"
         autoSend
       >
@@ -97,7 +97,7 @@ const ThreadWelcomeSuggestions: FC = () => {
       </ThreadPrimitive.Suggestion>
       <ThreadPrimitive.Suggestion
         className="hover:bg-muted/80 flex max-w-sm grow basis-0 flex-col items-center justify-center rounded-lg border p-3 transition-colors ease-in"
-        prompt="What is assistant-ui?"
+        prompt="What is NLP?"
         method="replace"
         autoSend
       >

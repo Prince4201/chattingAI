ddd34524cbb2f96782a8c2abec903feb7471e38d
Prince Jaiswal <pj1308109@gmail.com>
2025-09-04 00:02:20 +0530

    Update app-sidebar.tsx

--- my-ai/components/app-sidebar.tsx
+++ my-ai/components/app-sidebar.tsx
@@ -42,13 +42,13 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
          
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <Link href="https://github.com/assistant-ui/assistant-ui" target="_blank">
+              <Link href="https://github.com/Prince4201" target="_blank">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                   <Github className="size-4" />
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
                   <span className="font-semibold">GitHub</span>
-                  <span className="">View Source</span>
+                  <span className="">View Developer </span>
                 </div>
               </Link>
             </SidebarMenuButton>
